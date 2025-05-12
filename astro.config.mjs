f91c697d73b7a339cceef17b92ef632bc4fe9501
shm <m11c3.sh@gmail.com>
2025-05-13 01:02:46 +0900

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react(), mdx()],
+  adapter: cloudflare()
+});
