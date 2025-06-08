55292e9837e7952952ae76fe857902bee753fab0
shm <m11c3.sh@gmail.com>
2025-06-08 17:49:08 +0900

    Fix: robots.txtの削除とリネーム処理を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,28 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     buildEnd() {
-      if (process.env.SKIP_ROBOTS === 'true') {
+      console.log("CF_PAGES_URL", process.env.CF_PAGES_URL)
+      if (process.env.CF_PAGES_URL === "https://hardviz.com") {
+        const robotsDevPath = path.resolve('dist/robots.develop.txt');
+
+        // 開発用の robots.develop.txt が存在する場合は削除
+        if (fs.existsSync(robotsDevPath)) {
+          fs.unlinkSync(robotsDevPath);
+          console.log('robots.develop.txt removed from dist.');
+        }
+      } else {
         const robotsPath = path.resolve('dist/robots.txt');
+        const robotsDevPath = path.resolve('dist/robots.develop.txt');
+
+        // 本番用の robots.txt を削除し、開発用の robots.develop.txt を本番用にリネーム
         if (fs.existsSync(robotsPath)) {
           fs.unlinkSync(robotsPath);
           console.log('robots.txt removed from dist.');
+        }
+
+        if (fs.existsSync(robotsDevPath)) {
+          fs.renameSync(robotsDevPath, robotsPath);
+          console.log('robots.develop.txt renamed to robots.txt in dist.');
         }
       }
     }
