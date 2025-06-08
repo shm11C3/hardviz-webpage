405e5b6f7fc232be7b0f11af5d8a1410c744b5fc
shm <m11c3.sh@gmail.com>
2025-06-08 18:17:08 +0900

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import fs from "node:fs";
-import path from "node:path";
 
 console.log("[BUILD INFO] PRODUCTION:", process.env.PRODUCTION);
-console.log("[BUILD INFO] DEBUG_ENV:", process.env.DEBUG_ENV);
+console.log("[BUILD INFO] NODE_ENV:", process.env.NODE_ENV);
 
 // https://astro.build/config
 export default defineConfig({
